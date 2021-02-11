76e4e7ce04d2840c81d1a2fef13c7b3a314ff582
Valentin Berman <62676400+berman00@users.noreply.github.com>
2021-02-11 17:04:09 -0300

    Corregi nombres de los modulos y setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='LEDino',
     version='1.0',
-    py_modules=['LEDino','LEDino-gui', 'LEDino-cli'],
+    py_modules=['LEDino', 'LEDino_gui', 'LEDino_cli'],
     install_requires=[
         'Click',
     ],
     entry_points='''
         [console_scripts]
-        ledino=LEDino:ledino
+        ledino=LEDino_cli:ledino
     ''',)
